b777f74c2ae229758229dcd01aabc4477f80ac0b
Eliseo Euan Pantoja <eliseo.euan@gmail.com>
2017-05-04 01:11:10 -0500

    Validación usuario

--- cm.mx.catalogo/cm.mx.catalogo/cm.mx.catalogo/Model/Rules/UsuarioVR.cs
+++ cm.mx.catalogo/cm.mx.catalogo/cm.mx.catalogo/Model/Rules/UsuarioVR.cs
@@ -37,11 +37,26 @@ namespace cm.mx.catalogo.Rules
             if (!Enum.TryParse(oUsuario.Tipo, out tipo)) _mensajes.Add("Ingrese el tipo de usuario");
             if (!oUsuario.Contrasena.Equals(oUsuario.VerificacionContrasena)) _mensajes.Add("La contraseña y la verificación no coincide");
             //if (string.IsNullOrEmpty(oUsuario.Nivel)) _mensajes.Add("Ingrese el nivel del usuario");
-            if (rUsuario.ExisteCorreo(oUsuario.Email)) _mensajes.Add("El correo ya ha sido registrado");
+            var origen = rUsuario.ExisteCorreo(oUsuario.Email);
+            if (!string.IsNullOrEmpty(origen))
+            {
+                _mensajes.Add("El correo ya ha sido registrado");
+                if (origen != oUsuario.Origen)
+                {
+                    _mensajes.Add("No puede usar este método de registro");
+                    if (origen == Origen.MOVIL.ToString()) _mensajes.Add("Inicie sessión con su correo y contraseña");
+                    else _mensajes.Add("inicie sesión con: " + origen);
+                }
+
+            }
             if (string.IsNullOrEmpty(oUsuario.Nombre)) _mensajes.Add("Ingrese el nombre del usuario");
-            DateTime ini = DateTime.Now.AddYears(-300);
-            DateTime fin = DateTime.Now.AddYears(-10);
-            if (oUsuario.FechaNacimiento <= ini || oUsuario.FechaNacimiento >= fin) _mensajes.Add("Ingrese una fecha válida");
+            DateTime ini = DateTime.Now.AddYears(-100);
+            DateTime fin = DateTime.Now.AddYears(-13);
+            if (oUsuario.Origen == Origen.MOVIL.ToString())
+            {
+                if (oUsuario.FechaNacimiento <= ini || oUsuario.FechaNacimiento >= fin) _mensajes.Add("Ingrese una fecha válida");
+            }
+
             return _mensajes.Count() == 0;
         }
     }
